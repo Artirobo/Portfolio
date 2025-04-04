de7c92c9323dfa5aa9dd4606d7c68d9d9c4f6854
Manish joshi <searchzuck2013@gmail.com>
2025-04-04 13:26:41 +0545

    new

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -9,12 +9,12 @@ export function Welcome() {
           <div className="w-[500px] max-w-[100vw] p-4">
             <img
               src={logoLight}
-              alt="React Router"
+              alt="React "
               className="block w-full dark:hidden"
             />
             <img
               src={logoDark}
-              alt="React Router"
+              alt="React "
               className="hidden w-full dark:block"
             />
           </div>
